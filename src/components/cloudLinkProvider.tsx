67d98b0e061b85668412bd80351b087a99d7254b
Dom Del Nano <ddelnano@gmail.com>
2024-07-11 22:12:49 +0000

    Use cookies to remember previously selected Cloud
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/components/cloudLinkProvider.tsx
+++ src/components/cloudLinkProvider.tsx
@@ -17,11 +17,22 @@
  */
 
 import * as React from 'react';
+import Cookies from 'js-cookie';
 
-const initialCloud = {
-  name: 'New Relic Cloud',
-  baseUrl: 'https://work.withpixe.ai',
-};
+const availableClouds = [
+  {
+    name: 'New Relic Cloud',
+    baseUrl: 'https://work.withpixie.ai',
+    cloudAddr: 'withpixie.ai',
+  },
+  {
+    name: 'Cosmic Cloud',
+    baseUrl: 'https://work.getcosmic.ai',
+    cloudAddr: 'getcosmic.ai',
+  },
+];
+
+const initialCloud = availableClouds[0];
 
 export const CloudLinkContext = React.createContext(
   {
@@ -30,8 +41,29 @@ export const CloudLinkContext = React.createContext(
     setSelectedCloud: (cloud) => { },
   },
 );
+const getDefaultOrSelectedCloud = () => {
+  if (!Cookies.get('consent')) {
+    return initialCloud;
+  }
+  const selectedCloud = Cookies.get('selected-cloud');
+  if (selectedCloud) {
+    return JSON.parse(selectedCloud);
+  }
+  return initialCloud;
+};
 export default function CloudLinkProvider({ children }) {
-  const [selectedCloud, setSelectedCloud] = React.useState(initialCloud);
+  const isBrowser = typeof window !== 'undefined';
+  const [
+    selectedCloud,
+    setSelectedCloudState,
+  ] = React.useState(isBrowser ? getDefaultOrSelectedCloud() : initialCloud);
+  const setSelectedCloud = (cloud) => {
+    setSelectedCloudState(cloud);
+    // If the user has not consented to cookies, do not set a default cloud.
+    if (Cookies.get('consent')) {
+      Cookies.set('selected-cloud', cloud);
+    }
+  };
   return (
     <CloudLinkContext.Provider
       value={{
